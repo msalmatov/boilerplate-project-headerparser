edbdf60971beb7e6f59fc6679f2e5e078eade797
Murod Almatov <46152499+msalmatov@users.noreply.github.com>
2022-06-25 11:24:10 +0000

    final code

--- index.js
+++ index.js
@@ -21,8 +21,15 @@ app.get("/", function (req, res) {
 
 
 // your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+app.get("/api/whoami", function (req, res) {
+  const language = req.headers["accept-language"];
+  const software = req.headers["user-agent"];
+  const ipaddress = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
+  res.json({
+    ipaddress,
+    language,
+    software
+  });
 });
 
 
